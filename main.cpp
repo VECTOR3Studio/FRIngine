3cae4e43ab932db2179d3fa8f89cc3d608895140
krnl_dev <simonkyselica1@gmail.com>
2025-09-26 15:26:32 +0200

    Implement Player class with movement and rendering functionality

--- main.cpp
+++ main.cpp
@@ -1,18 +1,53 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-// <variant> is not strictly needed if you use sf::Event::is and sf::Event::getIf
-// #include <variant>
+
+class Player
+{
+    public:
+        Player(float x, float y)
+        {
+            m_shape.setPointCount(3);
+            m_shape.setRadius(30.f);
+            m_shape.setFillColor(sf::Color::Cyan);
+            m_pos = {x, y};
+            m_speed = 200.f;
+        }
+
+        void handleInput()
+        {
+            sf::Vector2f velocity(0.f, 0.f);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) velocity.y = -1;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) velocity.y = 1;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) velocity.x = -1;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) velocity.x = 1;
+
+            m_velocity = velocity;
+        }
+
+        void update(float dt)
+        {
+            m_pos += m_velocity * m_speed * dt;
+            m_shape.setPosition(m_pos);
+        }
+
+        void draw(sf::RenderWindow& window)
+        {
+            window.draw(m_shape);
+        }
+
+    private:
+        sf::Vector2f m_velocity;
+        sf::CircleShape m_shape;
+        sf::Vector2f m_pos;
+        float m_speed;
+};
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "FRIngine");
+    window.setFramerateLimit(60);
 
-    sf::CircleShape playerShape(50.f);
-
-    playerShape.setFillColor(sf::Color::Green);
-
-    sf::Vector2f playerPosition(400.f, 300.f);
-    float playerSpeed = 100.f;
+    Player player(400.f, 300.f);
 
     sf::Clock deltaClock;
 
@@ -29,29 +64,11 @@ int main()
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-        {
-            playerPosition.y -= playerSpeed * dt;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-        {
-            playerPosition.y += playerSpeed * dt;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-        {
-            playerPosition.x -= playerSpeed * dt;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-        {
-            playerPosition.x += playerSpeed * dt;
-        }
-
-        playerShape.setPosition(playerPosition);
+        player.handleInput();
+        player.update(dt);
 
         window.clear(sf::Color::Black);
-
-        window.draw(playerShape);
-
+        player.draw(window);
         window.display();
         
     }
